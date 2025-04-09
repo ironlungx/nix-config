ea77f66ce381a026580754701875aafcf9226b55
ironlungx <hwlooverhello@gmail.com>
2025-04-09 14:12:41 +0100

    changed some dunst stuff

--- home-manager/modules/hyprland/dunst.nix
+++ home-manager/modules/hyprland/dunst.nix
@@ -1,33 +1,42 @@
-{ config, pkgs, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
-    oranchelo-icon-theme
+    adwaita-icon-theme
   ];
   services.dunst = {
     enable = true;
     settings = {
-      font = "${config.stylix.fonts.monospace.name} 8"; ####
-      frame_width = 2;
-      separator_height = 2;
-      padding = 10;
-      horizontal_padding = 20;
-      line_height = 0;
-      transparency = 5;
-      corner_radius = 6;
-      dmenu = "${pkgs.wofi}/bin/wofi -dmenu"; 
-      origin = "bottom-center";
-      icon_theme = "Oranchelo";
-      enable_recursive_icon_lookup = true;
+      global = {
+        frame_width = 2;
+        separator_height = 2;
+        padding = 10;
+        horizontal_padding = 20;
+        line_height = 0;
+        transparency = 5;
+        corner_radius = 6;
+        dmenu = "${pkgs.wofi}/bin/wofi -dmenu";
+        origin = "bottom-center";
+        icon_theme = "Adwaita";
+        enable_recursive_icon_lookup = true;
 
-      # Background color
-      background = "#${config.lib.stylix.colors.base00}";
-      # Foreground color;
-      foreground = "#${config.lib.stylix.colors.base05}";
-      # Frame color;
-      frame_color = "#${config.lib.stylix.colors.base0D}";
-      # Separator color;
-      separator_color = "#${config.lib.stylix.colors.base07}";
-      # Highlight color (for URLs and actions);
-      highlight = "#${config.lib.stylix.colors.base0E}";
+        # # Background color
+        # background = config.lib.stylix.colors.withHashtag.base00;
+        #
+        # # Foreground color;
+        # foreground = config.lib.stylix.colors.withHashtag.base05;
+        #
+        # # Frame color;
+        # frame_color = config.lib.stylix.colors.withHashtag.base0D;
+        #
+        # # Separator color;
+        # separator_color = config.lib.stylix.colors.withHashtag.base07;
+        #
+        # # Highlight color (for URLs and actions);
+        # highlight = config.lib.stylix.colors.base0E;
+      };
     };
   };
 }
