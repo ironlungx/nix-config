1327e306effe2eff1797aff856b4331283548ad8
ironlungx <hwlooverhello@gmail.com>
2025-04-22 20:42:17 +0100

    starship added nix-shell indicator

--- home-manager/modules/shell/starship.nix
+++ home-manager/modules/shell/starship.nix
@@ -4,9 +4,8 @@
     settings = {
       scan_timeout = 10;
       add_newline = false;
-      format = "$directory $character";
+      format = "$directory $nix_shell $character";
       right_format = "$cmd_duration";
-
       character = {
         success_symbol = "[ ](bold green)";
         error_symbol = "[ ](bold red)";
@@ -14,33 +13,28 @@
         vimcmd_replace_symbol = "[ ](bold red)";
         vimcmd_replace_one_symbol = "[ ](bold red)";
       };
-
       username = {
         show_always = true;
         style_user = "bold bg:none fg:blue";
         format = "[$user]($style)";
       };
-
       hostname = {
         disabled = true;
         ssh_only = false;
         style = "bold bg:none fg:text";
         format = "@[$hostname]($style) ";
       };
-
       directory = {
         read_only = " ";
         truncation_length = 3;
         truncation_symbol = "./";
         style = "bold bright-purple";
       };
-
       git_branch = {
         format = " on [$symbol$branch(:$remote_branch)]($style) ";
         symbol = " ";
         style = "bold cyan";
       };
-
       cmd_duration = {
         min_time = 2000;
         show_milliseconds = false;
@@ -48,8 +42,17 @@
         style = "bold yellow";
         disabled = false;
       };
-
       line_break = {
+        disabled = false;
+      };
+      # --- nix_shell support added below ---
+      nix_shell = {
+        format = "[$symbol$name]($style) ";
+        symbol = " ";
+        style = "bold cyan";
+        impure_msg = "impure";
+        pure_msg = "pure";
+        unknown_msg = "unknown";
         disabled = false;
       };
     };
