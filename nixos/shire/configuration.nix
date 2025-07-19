d69cabda262a6fefbaa2065550dc5059e3d85504
ironlungx <hwlooverhello@gmail.com>
2025-07-19 17:32:46 +0100

    added git

--- nixos/shire/configuration.nix
+++ nixos/shire/configuration.nix
@@ -56,6 +56,7 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     rsync
+    git
   ];
   services.openssh.enable = true;
 
