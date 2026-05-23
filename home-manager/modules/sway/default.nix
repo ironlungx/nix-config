60c9086efa97d6faa43ff1e7a7bc07f36c3b1658
ironlungx <hwlooverhello@gmail.com>
2026-05-23 13:53:36 +0100

    updated flake
    swayy

---
+++ home-manager/modules/sway/default.nix
@@ -0,0 +1,61 @@
+{ home, pkgs, ... }:
+{
+  wayland.windowManager.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+
+    config = rec {
+      modifier = "Mod4";
+      terminal = "ghostty";
+      bars = [ ];
+
+      # Gaps (inner + outer)
+      gaps.inner = 8;
+      gaps.outer = 4;
+
+      # Optional: smart gaps/borders
+      gaps.smartGaps = true;
+      gaps.smartBorders = "on";
+
+      # Border width (set to 0 for no border)
+      window.border = 2;
+
+      startup = [
+        { command = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch cliphist store"; }
+        { command = "${pkgs.wl-clipboard}/bin/wl-paste --type image --watch cliphist store"; }
+        { command = "${pkgs.hyprpaper}/bin/hyprpaper"; }
+        { command = "${pkgs.dunst}/bin/dunst"; }
+        { command = "swayosd-server"; }
+        { command = "blueman-applet"; }
+        { command = "${pkgs.wayscriber}/bin/wayscriber --daemon"; }
+        # Optional: enable Waybar instead
+        # { command = "waybar"; }
+      ];
+    };
+
+    extraConfig = ''
+      set $menu "rofi -show drun"
+      set $browser "zen-beta"
+      set $screenshot "grim -g \\$\\(slurp\\) - \\| wl-copy"
+
+      bindsym Mod4+p exec $menu
+      bindsym Mod4+m exec $screenshot
+      bindsym Mod4+Control+Return exec $browser
+
+      bindsym XF86AudioRaiseVolume exec "${pkgs.swayosd}/bin/swayosd-client --output-volume=+5"
+      bindsym XF86AudioLowerVolume exec "${pkgs.swayosd}/bin/swayosd-client --output-volume=-5"
+      bindsym XF86AudioMute exec "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
+      bindsym XF86AudioNext exec "playerctl next"
+      bindsym XF86AudioPrev exec "playerctl previous"         
+      bindsym XF86AudioPause exec "playerctl play-pause"
+      bindsym XF86AudioPlay exec "playerctl play-pause"
+
+      input type:keyboard {
+
+        repeat_delay 200
+        repeat_rate 30
+      }
+    '';
+  };
+}
