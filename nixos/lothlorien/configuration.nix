fe45cccb8442e9d1b42ced80716747c4098779e0
ironlungx <hwlooverhello@gmail.com>
2026-08-14 10:54:10 +0530

    firmware updates

--- nixos/lothlorien/configuration.nix
+++ nixos/lothlorien/configuration.nix
@@ -31,6 +31,7 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  services.fwupd.enable = true;
   services.resolved.enable = true;
   # Set your time zone.
   time.timeZone = "Europe/London";
